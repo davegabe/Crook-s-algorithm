ce5e2bd5f58ad472581396a05b13d96cc18c7527
Davide <me@davegabe.it>
2022-01-18 13:54:42 +0100

    Refactoring and added isSolved method

--- crook.c
+++ crook.c
@@ -19,7 +19,7 @@ typedef struct cell
 cell **readSudoku(int *n, list ***possRows, list ***possColumns, list ***possGrids, const char *filename)
 {
     FILE *fp = fopen(filename, "r");
-    cell **matrix = NULL, **tmp;
+    cell **sudoku = NULL, **tmp;
     char line[1024];
 
     // read the size of the sudoku
@@ -27,7 +27,7 @@ cell **readSudoku(int *n, list ***possRows, list ***possColumns, list ***possGri
     {
         char *scan = line;
         sscanf(scan, "%d", n);
-        matrix = malloc(*n * sizeof(matrix));
+        sudoku = malloc(*n * sizeof(sudoku));
 
         //create the possible values list for each row
         *possRows = malloc(*n * sizeof(list *));
@@ -90,12 +90,12 @@ cell **readSudoku(int *n, list ***possRows, list ***possColumns, list ***possGri
     // read the sudoku
     for (size_t i = 0; i < *n; ++i)
     {
-        matrix[i] = calloc(*n, sizeof(cell));
+        sudoku[i] = calloc(*n, sizeof(cell));
         for (size_t j = 0; j < *n; ++j)
         {
             int t;
             fscanf(fp, "%d", &t);
-            (matrix[i] + j)->val = t; //value of cell is the same as the value in the file
+            (sudoku[i] + j)->val = t; //value of cell is the same as the value in the file
             if (t == 0)               //if it's a void cell (has value 0)
             {
                 //create a list of all possible values (from 1 to 9)
@@ -111,19 +111,19 @@ cell **readSudoku(int *n, list ***possRows, list ***possColumns, list ***possGri
                 }
 
                 //add all possible values (from 1 to 9)
-                (matrix[i] + j)->poss = last;
+                (sudoku[i] + j)->poss = last;
             }
             else //if the cell has a value
             {
                 //possible values list is empty
-                (matrix[i] + j)->poss = NULL;
+                (sudoku[i] + j)->poss = NULL;
             }
         }
     }
 
     fclose(fp);
 
-    return matrix;
+    return sudoku;
 }
 
 void printPossList(list *l)
@@ -137,7 +137,7 @@ void printPossList(list *l)
     printf("%d \n", tmp->val);
 }
 
-void printSudokuDebug(cell **matrix, list **possRows, list **possColumns, list **possGrids, int n, int possibleValues)
+void printSudokuDebug(cell **sudoku, list **possRows, list **possColumns, list **possGrids, int n, int possibleValues)
 {
     printf("\n#############################\nSUDOKU ");
     if (possibleValues == 0)
@@ -155,7 +155,7 @@ void printSudokuDebug(cell **matrix, list **possRows, list **possColumns, list *
         {
             for (size_t j = 0; j < n; ++j)
             {
-                printf("%d ", (matrix[i] + j)->val);
+                printf("%d ", (sudoku[i] + j)->val);
             }
             printf("\n");
         }
@@ -163,14 +163,14 @@ void printSudokuDebug(cell **matrix, list **possRows, list **possColumns, list *
         {
             for (size_t j = 0; j < n; ++j)
             {
-                if ((matrix[i] + j)->poss == NULL)
+                if ((sudoku[i] + j)->poss == NULL)
                 {
-                    printf("(%d,%d) = %d \n", i, j, (matrix[i] + j)->val);
+                    printf("(%d,%d) = %d \n", i, j, (sudoku[i] + j)->val);
                 }
                 else
                 {
                     printf("(%d,%d) = ", i, j);
-                    printPossList((matrix[i] + j)->poss);
+                    printPossList((sudoku[i] + j)->poss);
                 }
             }
             printf("Row %d = ", i);
@@ -241,31 +241,56 @@ int getIndexpossGrids(int n, int i, int j)
     return i / rowN + (j / rowN) * rowN;
 }
 
+int isSolved(cell **sudoku, int n)
+{
+    for (size_t i = 0; i < n; ++i)
+    {
+        for (size_t j = 0; j < n; ++j)
+        {
+            if ((sudoku[i] + j)->val == 0)
+            {
+                return 0;
+            }
+        }
+    }
+    return 1;
+}
+
 int main(void)
 {
     int n = 0;
     list **possRows = NULL;
     list **possColumns = NULL;
     list **possGrids = NULL;
-    cell **matrix = readSudoku(&n, &possRows, &possColumns, &possGrids, "sudoku.txt");
+    cell **sudoku = readSudoku(&n, &possRows, &possColumns, &possGrids, "sudoku.txt");
 
-    if (matrix == NULL)
+    if (sudoku == NULL)
     {
-        fprintf(stderr, "could not read matrix\n");
+        fprintf(stderr, "could not read sudoku\n");
         return 1;
     }
     else
     {
-        //printSudokuDebug(matrix, possRows, possColumns, possGrids, n, 0);
-        printSudokuDebug(matrix, possRows, possColumns, possGrids, n, 1);
+        //printSudokuDebug(sudoku, possRows, possColumns, possGrids, n, 0);
+        printSudokuDebug(sudoku, possRows, possColumns, possGrids, n, 1);
     }
     list *node = NULL;
     list *prev = NULL;
-    if (findFromList(matrix[0][0].poss, 2, &node, &prev) == 1)
+    if (findFromList(sudoku[0][0].poss, 2, &node, &prev) == 1)
     {
-        removeFromList(&matrix[0][0].poss, &node, &prev);
+        removeFromList(&sudoku[0][0].poss, &node, &prev);
     }
-    printSudokuDebug(matrix, possRows, possColumns, possGrids, n, 1);
+    printSudokuDebug(sudoku, possRows, possColumns, possGrids, n, 1);
+
+    if(isSolved(sudoku, n) == 1)
+    {
+        printf("Sudoku solved!\n");
+    }
+    else
+    {
+        printf("Sudoku not solved!\n");
+    }
+
     //TODO: freeing memory
 
     return 0;
